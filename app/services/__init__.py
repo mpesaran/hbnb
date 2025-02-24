f8dd3090ee98c893a65e52a47717e92bd6838eb8
aavly <annavyly@gmail.com>
2025-02-24 15:20:36 +1100

    plan for facade pattern with placeholders in services/ sub

---
+++ app/services/__init__.py
@@ -0,0 +1,7 @@
+from app.services.facade import HBnBFacade
+
+# Creating an instance of the HBnBFacade class 
+facade = HBnBFacade()
+
+# facade object will be used as a singleton to ensure only 1 instance
+# ...of the HBnBFacade class is created and used thruout the application.
