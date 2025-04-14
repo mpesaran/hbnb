a9a7116b737cd605cd229addcce11a0a82ea6a9e
Crystal-holberton <9188@holbertonstudents.com>
2025-04-14 10:23:14 +1000

    revert

--- part-04/scripts.js
+++ part-04/scripts.js
@@ -1,10 +1,3 @@
-document.getElementById('login-form')?.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const email = e.target.email.value;
-  const password = e.target.password.value;
-  // Simulate login
-  if (email && password) {
-    localStorage.setItem('loggedIn', 'true');
-    window.location.href = 'index.html';
-  }
+document.addEventListener('DOMContentLoaded', () => {
+  /* DO SOMETHING */
 });
